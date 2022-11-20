229e526ada3269d54d254938baa6741cec1bb049
Ian Waddington <iwaddo@icloud.com>
2022-11-20 16:27:24 +0000

    Initial Commit

---
+++ D25 Challenge/D25_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  D25_ChallengeApp.swift
+//  D25 Challenge
+//
+//  Created by Ian Waddington on 20/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct D25_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
